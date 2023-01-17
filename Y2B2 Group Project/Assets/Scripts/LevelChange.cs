53e1c1ead0af1bf4abc0cd8666a6406bfa7fff1b
AlexVelicea <veliceaalex1@gmail.com>
2023-01-17 14:47:33 +0100

    Fluid system
    liquids

---
+++ Y2B2 Group Project/Assets/Scripts/LevelChange.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelChange : MonoBehaviour
+{
+    [SerializeField] float liquidLevel;
+    [SerializeField] GameObject flask;
+
+    private void FixedUpdate()
+    {
+        if (flask.transform.rotation.eulerAngles.z > 90 && flask.transform.rotation.eulerAngles.z < 270)
+        {
+            if(liquidLevel > -0.11f)
+            {
+                liquidLevel = liquidLevel - 0.004f;
+                GetComponent<MeshRenderer>().material.SetFloat("FluidLevel", liquidLevel);
+            }
+        }
+
+    }
+}
