0317ad8fcbd8e6e01436e0ab2978d289445d12e0
EdgarKnauer <95930445+EdgarKnauer@users.noreply.github.com>
2023-01-17 11:17:56 +0100

    Fixed some buggs with grabbing + assigned materials to prefabs

--- Y2B2 Group Project/Assets/Edgar/Scripts/PlayerCharacter/PlayerController.cs
+++ Y2B2 Group Project/Assets/Edgar/Scripts/PlayerCharacter/PlayerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.InputSystem;
+using Unity.XR.CoreUtils;
 
 
 public class PlayerController : MonoBehaviour
@@ -28,26 +29,21 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private GameObject leftHand;
     [SerializeField] private GameObject rightHand;
 
+    private Vector2 inputAxis;
+    [SerializeField] XRNode leftController;
+    [SerializeField] private XROrigin rig;
+    [SerializeField] private float movementSpeed = 2f;
+
+    private bool MBPressed;
+    public bool checkBool1;
+
     private void Awake()
     {
         gameManager = FindObjectOfType<GameManager>();
         musicManager = FindObjectOfType<MusicManager>();
         NBF = FindObjectOfType<NavigationButtonFunctionality>();
     }
-
-    public void DeActivateLineRenderers(bool activate)
-    {
-        if(activate)
-        {
-            leftHand.GetComponent<UnityEngine.XR.Interaction.Toolkit.XRInteractorLineVisual>().lineLength = 10;
-            rightHand.GetComponent<UnityEngine.XR.Interaction.Toolkit.XRInteractorLineVisual>().lineLength = 10;
-        }
-        else
-        {
-            leftHand.GetComponent<UnityEngine.XR.Interaction.Toolkit.XRInteractorLineVisual>().lineLength = 0;
-            rightHand.GetComponent<UnityEngine.XR.Interaction.Toolkit.XRInteractorLineVisual>().lineLength = 0;
-        }        
-    }
+        
 
     private void OnEnable()
     {
@@ -61,9 +57,10 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        UnityEngine.XR.InputDevice leftControllerDevice = InputDevices.GetDeviceAtXRNode(leftController);
+        leftControllerDevice.TryGetFeatureValue(UnityEngine.XR.CommonUsages.primary2DAxis, out inputAxis);
+        leftControllerDevice.TryGetFeatureValue(UnityEngine.XR.CommonUsages.menuButton, out MBPressed);
 
-        //leftController.TryGetFeatureValue(CommonUsages.triggerButton, out leftButtonPressed);
-        //rightController.TryGetFeatureValue(CommonUsages.triggerButton, out rightButtonPressed);
         switch (currentGameState)
         {
             case "StartUp":
@@ -82,9 +79,11 @@ public class PlayerController : MonoBehaviour
                 //{
                 //    gameManager.UpdateGameState(GameManager.GameStates.Navigation);
                 //};
-
-                if(Input.GetKeyUp("n"))
+                Debug.Log(MBPressed + " MB pressed");
+                Debug.Log(checkBool1 +" Checkbool");
+                if(MBPressed && checkBool1)
                 {
+                    checkBool1 = false;
                     StartCoroutine(OpeningNavigation());
                     Debug.Log("SwitchedGameStateGamePLay");
                     DeActivateLineRenderers(true);
@@ -103,16 +102,21 @@ public class PlayerController : MonoBehaviour
                 //    gameManager.UpdateGameState(GameManager.GameStates.GamePlay);
                 //};
 
-                if (Input.GetKeyUp("n"))
+                if (MBPressed && checkBool1)
                 {
+                    checkBool1 = false;
+                    StartCoroutine(ClosingNavigation());
                     Debug.Log("SwitchedGameStateNavigation");
-                    openingNavigation = false;
-                    teleporting = false;
                     DeActivateLineRenderers(false);
                     gameManager.UpdateGameState(GameManager.GameStates.GamePlay);
                 }
                 break;
         }
+
+        Quaternion headDirection = Quaternion.Euler(0, rig.Camera.transform.eulerAngles.y, 0);
+        Vector3 direction = headDirection * new Vector3(inputAxis.x, 0, inputAxis.y);
+        rig.transform.Translate(direction * Time.deltaTime * movementSpeed);
+
     }
 
     
@@ -148,6 +152,29 @@ public class PlayerController : MonoBehaviour
         NBF.playClip(clip);
         yield return new WaitForSecondsRealtime(clip.length);
         openingNavigation = false;
+        checkBool1 = true;
+    }
+
+    private IEnumerator ClosingNavigation()
+    {
+        yield return new WaitForSecondsRealtime(1);
+        teleporting = false;
+        openingNavigation = false;
+        checkBool1 = true;
+    }
+
+    public void DeActivateLineRenderers(bool activate)
+    {
+        if (activate)
+        {
+            leftHand.GetComponent<UnityEngine.XR.Interaction.Toolkit.XRInteractorLineVisual>().lineLength = 10;
+            rightHand.GetComponent<UnityEngine.XR.Interaction.Toolkit.XRInteractorLineVisual>().lineLength = 10;
+        }
+        else
+        {
+            leftHand.GetComponent<UnityEngine.XR.Interaction.Toolkit.XRInteractorLineVisual>().lineLength = 0;
+            rightHand.GetComponent<UnityEngine.XR.Interaction.Toolkit.XRInteractorLineVisual>().lineLength = 0;
+        }
     }
 
     private void OnDisable()
